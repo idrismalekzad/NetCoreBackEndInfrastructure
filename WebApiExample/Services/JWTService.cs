b0608c85fe294a55fbe105c391219644a96ce656
Idris Malekzad <idrismalekzad@gmail.com>
2024-03-02 23:30:46 +0330

    Update JWTService.cs

--- WebApiExample/Services/JWTService.cs
+++ WebApiExample/Services/JWTService.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -6,22 +8,27 @@ using System.Text;
 using WebApiExample.Controllers;
 using WebApiExample.Data.Entities;
 
-namespace WebApiExample.Services
+namespace WebApiExample.Services.JWT
 {
     public class JWTService : IJWTService
     {
         private const string TokenSecret = "RadinPeymentChannelUserManagementSecurely";
         private readonly TimeSpan TokenLifeTime = TimeSpan.FromHours(8);
 
+        private readonly UserManager<ApplicationUser> _userManager;
+        public JWTService(UserManager<ApplicationUser> userManager)
+        {
+                _userManager = userManager;
+        }
+
         public async Task<string> GenerateToken(ApplicationUser user)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
-            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(TokenSecret));
-
+            var key = Encoding.ASCII.GetBytes(TokenSecret);
             var claims = new List<Claim>()
             {
                 //new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id),
                 new Claim(JwtRegisteredClaimNames.Email, user.Email),
                 //new Claim("userid", user.Id.ToString())
             };
@@ -30,14 +37,25 @@ namespace WebApiExample.Services
             {
                 Subject = new ClaimsIdentity(claims),
                 Expires = DateTime.UtcNow.Add(TokenLifeTime),
-                Issuer = "https://radin.tech/",
-                Audience = "https://usermangement.channel.com/",
-                SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256)
+                //Issuer = "https://radin.tech/",
+                //Audience = "https://usermangement.channel.com/",
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+
             };
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
             var jwt = tokenHandler.WriteToken(token);
             return jwt;
+        }
+
+        public async Task<ApplicationUser?> GetUserByID(string userId)
+        {
+            var rtn = await _userManager.FindByIdAsync(userId);
+            if (rtn == null)
+            {
+                return null;
+            }
+            return rtn;
         }
     }
 }
